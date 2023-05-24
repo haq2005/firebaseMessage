3528a4e617362e2d733e723553bfd3b16f5eb060
haq2005 <amakathar@gmail.com>
2023-05-24 18:11:10 +0545

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -5,7 +5,7 @@ importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.5.0/firebase-messaging.js');
 
 if ('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('../firebase-messaging-sw.js')
+    navigator.serviceWorker.register('./firebase-messaging-sw.js')
       .then(function(registration) {
         console.log('Registration successful, scope is:', registration.scope);
       }).catch(function(err) {
